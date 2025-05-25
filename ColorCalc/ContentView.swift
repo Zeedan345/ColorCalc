d12f8bbc8b6fddf545547bf60f79303226fb55e2
Zeedan345 <zeedan345@gmail.com>
2025-05-25 17:11:57 -0500

    added button and green calc

--- ColorCalc/ContentView.swift
+++ ColorCalc/ContentView.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var model = FrameHandler()
-    
+    @State private var isCapturing = false
+    @State private var isRecording = false
+    @State private var showChart = false
+
     var body: some View {
-        FrameView(image: model.frame)
-            .ignoresSafeArea()
+        ZStack {
+            FrameView(image: model.frame)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                
+                Button(action: {
+                    if isRecording {
+                        isCapturing = false
+                        showChart = true
+                    } else {
+                        model.startCapturing()
+                        isCapturing = true
+                        showChart = false
+                    }
+                    isRecording.toggle()
+                }) {
+                    Text(isRecording ? "Stop" : "Record")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(isRecording ? Color.gray : Color.red)
+                        .clipShape(Capsule())
+                }
+                .padding(.bottom, 40)
+            }
+            if showChart {
+                
+            }
+        }
     }
 }
 
--- ColorCalc/FrameHandler.swift
+++ ColorCalc/FrameHandler.swift
@@ -10,7 +10,10 @@ import CoreImage
 
 class FrameHandler: NSObject, ObservableObject {
     @Published var frame: CGImage?
+    @Published var greenValues: [CGFloat] = []
+    
     private var permissionGranted = false
+    private var isCapturing = false
     private let captureSession = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
     private let context = CIContext()
@@ -22,6 +25,17 @@ class FrameHandler: NSObject, ObservableObject {
             self.setupCaptureSession()
             self.captureSession.startRunning()
         }
+    }
+    
+    func startCapturing() {
+        greenValues = []
+        isCapturing = true
+    }
+    func stopCapturing() {
+        isCapturing = false
+    }
+    func resetGreenValues() {
+        greenValues = []
     }
     
     func checkPermission() {
@@ -62,9 +76,14 @@ extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let cgImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
         
+        let avgGreen = averageGreen(in: cgImage)
+        
         // All UI updates should be/ must be performed on the main queue.
         DispatchQueue.main.async { [unowned self] in
             self.frame = cgImage
+            if isCapturing {
+                self.greenValues.append(avgGreen)
+            }
         }
     }
     
@@ -74,5 +93,24 @@ extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
         guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
         
         return cgImage
+    }
+    private func averageGreen(in cgImage: CGImage) -> CGFloat {
+        guard let data = cgImage.dataProvider?.data,
+              let ptr = CFDataGetBytePtr(data) else {return 0}
+        
+        let bytesPerPixel = cgImage.bitsPerPixel/8
+        let width = cgImage.width
+        let height = cgImage.height
+        let pixelCount = width*height
+        
+        var greenTotal: CGFloat = 0
+        
+        for i in 0..<pixelCount{
+            let pixelOffset = i * bytesPerPixel
+            let g = CGFloat(ptr[pixelOffset + 1])
+            greenTotal += g
+        }
+        
+        return greenTotal / CGFloat(pixelCount)
     }
 }
