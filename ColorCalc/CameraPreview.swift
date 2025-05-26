41d24c49febeabf63cee239340d471e79bb013b8
Zeedan345 <zeedan345@gmail.com>
2025-05-26 13:14:35 -0500

    added icon and fullscreen

---
+++ ColorCalc/CameraPreview.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPreview.swift
+//  ColorCalc
+//
+//  Created by Zeedan Feroz Khan on 5/26/25.
+//
+
+import SwiftUI
+
+struct CameraPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPreview()
+}
