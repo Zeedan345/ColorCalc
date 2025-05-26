a7d5ead16a99f391795ca50a0d6993e0ecece772
Zeedan345 <zeedan345@gmail.com>
2025-05-26 12:17:11 -0500

    added chart

---
+++ ColorCalc/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  ColorCalc
+//
+//  Created by Zeedan Feroz Khan on 5/26/25.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
